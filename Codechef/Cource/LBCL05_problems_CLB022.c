19660d4ee7abe3a95a5fafa44e6b099026adb04a
utsab-2023-55 <utsabsantra@gmail.com>
2024-03-18 20:59:22 +0530

    code 7

---
+++ Codechef/Cource/LBCL05_problems_CLB022.c
@@ -0,0 +1,8 @@
+// Create a variable in editor named age and assign the value 25 to it.
+// Print the value of age variable using print statement.
+# include <stdio.h>
+int main(){
+     int age =25;
+    printf("%d", age);
+    return 0;
+}
