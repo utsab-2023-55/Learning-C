e9a3f3cb306062fae8bc89fb8c109c08a8efeb0b
utsab-2023-55 <utsabsantra@gmail.com>
2024-03-18 20:51:00 +0530

    code 6

---
+++ Codechef/Cource/LBCL04_problems_CLB018.c
@@ -0,0 +1,11 @@
+// Write a program which does the following
+
+// Print the result of subtraction of 9 and 2
+// Print the result of division of 30 by 5
+// Output both results on separate lines
+# include <stdio.h>
+int main(){
+    	printf("%d\n",9-2);
+	    printf("%d",30/5);
+    return 0;
+}
