fe54f8933095c5171e0ae47e890abdc0274143e7
utsab-2023-55 <utsabsantra@gmail.com>
2024-03-18 20:27:56 +0530

    testing

---
+++ Codechef/Cource/LBCL02_problems_CLB007.c
@@ -0,0 +1,14 @@
+// Write a program which does the following
+
+// Add a printf statement and output the sum of 3 + 4
+// Add another printf statement and output the sum of 2 + 1.
+// Use the printf syntax we learned in last problem.
+
+// Note - Notice that in the output they are printed together without any space between them.
+# include <stdio.h>
+int main(){
+    int a=3;int b=4;int c=2;int d=1;
+    printf("%d",a+b);
+    printf("%d",c+d);
+    return 0;
+}
