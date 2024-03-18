ba6c6c7c259f9efe0532216f7615021e91bc9eb5
utsab-2023-55 <utsabsantra@gmail.com>
2024-03-18 21:07:23 +0530

    code 9

---
+++ Codechef/Cource/LBCL05_problems_CLB025.c
@@ -0,0 +1,12 @@
+// Find out and display the area of a rectangle of sides 45 and 76 respectively.
+// Declare variables length, width and area and assign the relevant values to them
+// Make sure to add some text, so that the person reading the output knows what the output means.
+# include <stdio.h>
+int main(){
+      int length = 45;
+  int width = 76;
+  int area = length * width;
+  printf("The Area of the given rectangle is %d",area);
+
+    return 0;
+}
