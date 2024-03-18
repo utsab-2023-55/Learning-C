26eb3a4b8d92fcaae6769aaba4685bba20648e89
utsab-2023-55 <utsabsantra@gmail.com>
2024-03-18 21:03:50 +0530

    code 8

---
+++ Codechef/Cource/LBCL05_problems_CLB024.c
@@ -0,0 +1,11 @@
+// Declare two integer variables a and b
+// Assign the value 23 to a and 20 to b
+// Output the sum of a and b
+# include <stdio.h>
+int main(){
+    int a,b;
+    a=23;
+    b=20;
+    printf("%d",a+b);
+    return 0;
+}
